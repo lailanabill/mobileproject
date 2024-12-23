eed97d1f7b87eaeea047376f66b42b5b7033f820
ShahdTamerr <125687625+shahdsoliman@users.noreply.github.com>
2024-12-23 23:29:44 +0200

    main

--- reqbot/lib/main.dart
+++ reqbot/lib/main.dart
@@ -1,35 +1,21 @@
 import 'package:flutter/material.dart';
-import 'package:reqbot/screens/record.dart';
-import 'package:reqbot/screens/sign_in_page.dart';
-import 'package:reqbot/screens/sign_up_page.dart';
-import 'package:reqbot/screens/upload_convert.dart';
-import 'package:reqbot/screens/welcome_page.dart';
-import 'package:reqbot/screens/home_screen.dart'; // Import the HomeScreen
+import 'package:provider/provider.dart'; // For state management
+import 'package:reqbotui/providers/favorites_provider.dart';
+import 'screens/record.dart';
+import 'screens/sign_in_page.dart';
+import 'screens/sign_up_page.dart';
+import 'screens/upload_convert.dart';
+import 'screens/welcome_page.dart';
+import 'screens/home_screen.dart';
 
 void main() {
-  runApp(const ReqBotApp());
-}
-
-class ReqBotApp extends StatelessWidget {
-  const ReqBotApp({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'ReqBot',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-        visualDensity: VisualDensity.adaptivePlatformDensity,
-      ),
-      initialRoute: '/',
-      routes: {
-        '/': (context) => WelcomePage(),
-        '/sign-in': (context) => SignInPage(),
-        '/sign-up': (context) => SignUpPage(),
-        '/HomeScreen': (context) => HomeScreen(), // Add HomeScreen route
-        '/record': (context) => Record(), // Add HomeScreen route
-      },
-    );
-  }
+  runApp(
+    MultiProvider(
+      providers: [
+        ChangeNotifierProvider(
+            create: (_) => FavoritesProvider()), // Register FavoritesProvider
+      ],
+      child: const ReqBotApp(),
+    ),
+  );
 }
