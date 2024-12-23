0f348f4f163b1e6542e3c3b1f47603757d0a0e20
ShahdTamerr <125687625+shahdsoliman@users.noreply.github.com>
2024-12-23 23:28:11 +0200

    favproviderss

--- reqbot/lib/providers/favorites_provider.dart
+++ reqbot/lib/providers/favorites_provider.dart
@@ -7,4 +7,14 @@ class FavoritesProvider extends ChangeNotifier {
 
   void toggleFavorite(String projectName) {
     if (_favoriteProjects.contains(projectName)) {
+      _favoriteProjects.remove(projectName);
+    } else {
+      _favoriteProjects.add(projectName);
+    }
+    notifyListeners();
+  }
 
+  bool isFavorite(String projectName) {
+    return _favoriteProjects.contains(projectName);
+  }
+}
