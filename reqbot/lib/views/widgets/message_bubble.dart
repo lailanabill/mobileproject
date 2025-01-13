3c660c9056fd0529f91d0ec3fb93e27bbcbf62ae
shahdibrahiim <Shahd2103789@miuegypt.edu.eg>
2025-01-13 20:57:25 +0200

    chat widget

---
+++ reqbot/lib/views/widgets/message_bubble.dart
@@ -0,0 +1,37 @@
+// lib/views/widgets/message_bubble.dart
+import 'package:flutter/material.dart';
+
+class MessageBubble extends StatelessWidget {
+  final String message;
+  final bool isUser;
+
+  const MessageBubble({
+    Key? key,
+    required this.message,
+    required this.isUser,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Align(
+      alignment: isUser ? Alignment.centerRight : Alignment.centerLeft,
+      child: Container(
+        margin: EdgeInsets.symmetric(vertical: 4, horizontal: 8),
+        padding: EdgeInsets.symmetric(vertical: 10, horizontal: 14),
+        decoration: BoxDecoration(
+          color: isUser ? Colors.blue : Colors.grey[200],
+          borderRadius: BorderRadius.circular(20),
+        ),
+        constraints: BoxConstraints(
+          maxWidth: MediaQuery.of(context).size.width * 0.7,
+        ),
+        child: Text(
+          message,
+          style: TextStyle(
+            color: isUser ? Colors.white : Colors.black,
+          ),
+        ),
+      ),
+    );
+  }
+}
